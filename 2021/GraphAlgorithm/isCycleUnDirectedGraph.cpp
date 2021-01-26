30cbfae57d006a76b28b0179284e148aff6b97f9
Anuj Vishwakarma <anujvishwakarmamnnit@gmail.com>
2021-01-26 22:05:16 +0530

    Practice

--- 2021/GraphAlgorithm/isCycleUnDirectedGraph.cpp
+++ 2021/GraphAlgorithm/isCycleUnDirectedGraph.cpp
@@ -6,6 +6,29 @@ void input(){
 	freopen("output.txt","w",stdout);
 }
 
+int find(int x, vector<int> &parent){
+	while(x!=parent[x]){
+		x=parent[x];
+	}
+	return x;
+}
+
+int merge(int x, int y, std::vector<int> &parent){
+	parent[y]=x;
+}
+
+bool isCycle(vector<pair<int,int>> &edges, int n, std::vector<int> &parent){
+	int i;
+	for(i=0;i<edges.size();i++){
+		int u = find(edges[i].first, parent);
+		int v = find(edges[i].second, parent);
+		if(u!=v){
+			merge(u,v, parent);
+		}else{
+			return true;
+		}
+	}
+}
 
 int main(int argc, char const *argv[]){
 	int i,j,k,l,m,n;
@@ -17,7 +40,11 @@ int main(int argc, char const *argv[]){
 		cin>>u>>v;
 		edges.push_back(make_pair(u,v));
 	}
-	bool isCyc = isCycle();
+	std::vector<int> parent;
+	for(i=0;i<n;i++){
+		parent.push_back(i);
+	}
+	bool isCyc = isCycle(edges, n, parent);
 	cout<<isCyc<<endl;
 	return 0;
 }
