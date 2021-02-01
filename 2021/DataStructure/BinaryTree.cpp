3d5e2fbdd317c617a859049e674398f3cc8decf5
Anuj Vishwakarma <anujvishwakarmamnnit@gmail.com>
2021-02-01 16:08:44 +0530

    Practice

--- 2021/DataStructure/BinaryTree.cpp
+++ 2021/DataStructure/BinaryTree.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/* Let us create following BST 
+              50 
+           /     \ 
+          30      70 
+         /  \    /  \ 
+       20   40  60   80 */
+
 void input(){
 	freopen("input.txt","r",stdin);
 	freopen("output.txt","w",stdout);
@@ -40,6 +47,49 @@ void inorder(struct node *root){
 	inorder(root->right);
 }
 
+void levelOrder(struct node *root){
+	int i;
+	queue<struct node *> myQueue;
+	myQueue.push(root);
+	while(!myQueue.empty()){
+		struct node *curr = myQueue.front();
+		myQueue.pop();
+		cout<<curr->data<<" ";
+		if(curr->left){
+			myQueue.push(curr->left);
+		}
+		if(curr->right){
+			myQueue.push(curr->right);
+		}
+	}
+}
+
+void inorderbyStack(struct node *curr){
+	stack<struct node *> myStack;
+
+	while(curr!= NULL || !myStack.empty()){
+		while(curr!= NULL){
+			myStack.push(curr);
+			curr = curr->left;
+		}
+		curr = myStack.top();
+		myStack.pop();
+		cout<<curr->data<<" ";
+		curr = curr->right;
+	}
+}
+
+int height(struct node *root){
+	if(root == NULL){
+		return 0;
+	}
+	int lefth = height(root->left);
+	int righth = height(root->right);
+
+	return 1+max(lefth, righth);
+}
+
+
 int main(int argc, char const *argv[]) {
 	input();
 	int i,j,k,m,n;
@@ -50,6 +100,17 @@ int main(int argc, char const *argv[]) {
 		root=insert(root,k);
 	}
 	//print the in-order of tree;
+	cout<<"-----------Inorder-------->"<<endl;
 	inorder(root);
+	cout<<endl;
+	cout<<"-------------------------->"<<endl;
+
+	//levelOrder;
+	//levelOrder(root);
+
+	//int h = height(root);
+	//cout<<"height is "<<h<<endl;
+	//
+	inorderbyStack(root);
 	return 0;
 }
