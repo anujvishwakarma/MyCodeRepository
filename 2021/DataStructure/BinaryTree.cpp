8ccfdcd65de43f1ff9c5c46d1b91faabecc77f0f
Anuj Vishwakarma <anujvishwakarmamnnit@gmail.com>
2021-02-01 15:22:01 +0530

    Practice- Tree

--- 2021/DataStructure/BinaryTree.cpp
+++ 2021/DataStructure/BinaryTree.cpp
@@ -6,7 +6,50 @@ void input(){
 	freopen("output.txt","w",stdout);
 }
 
-int main(int argc, char const *argv[]){
-	
+struct node {
+	int data;
+	struct node *left;
+	struct node *right;
+};
+
+struct node *insert(struct node *root, int data){
+	int i,j;
+	struct node *tmp;
+	if(root == NULL){
+		tmp = (struct node *)malloc(sizeof(struct node));
+		tmp->data = data;
+		tmp->left = NULL;
+		tmp->right = NULL;
+		return tmp;
+	}
+	else if(root->data < data){
+		root->right = insert(root->right, data); 
+	}
+	else{
+		root->left = insert(root->left, data);
+	}
+	return root;
+}
+
+void inorder(struct node *root){
+	if(root == NULL){
+		return;
+	}
+	inorder(root->left);
+	cout<<root->data<<" ";
+	inorder(root->right);
+}
+
+int main(int argc, char const *argv[]) {
+	input();
+	int i,j,k,m,n;
+	cin>>n;
+	struct node *root = NULL;
+	for(i=0;i<n;i++){
+		cin>>k;
+		root=insert(root,k);
+	}
+	//print the in-order of tree;
+	inorder(root);
 	return 0;
 }
